d20244c5f0e1059d339e20b1b83f2f5abddab7e4
Nicholas C. Zakas <nicholas@nczconsulting.com>
2014-12-06 14:45:50 -0800

    Update: Bring in Makefile.js for linting and browserify

---
+++ Makefile.js
@@ -0,0 +1,262 @@
+/**
+ * @fileoverview Build file
+ * @author nzakas
+ */
+/*global cat, cp, echo, exec, exit, find, mkdir, mv, rm, target, test*/
+
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+require("shelljs/make");
+
+var checker = require("npm-license"),
+    dateformat = require("dateformat"),
+    nodeCLI = require("shelljs-nodecli");
+
+//------------------------------------------------------------------------------
+// Settings
+//------------------------------------------------------------------------------
+
+var OPEN_SOURCE_LICENSES = [
+    /MIT/, /BSD/, /Apache/, /ISC/, /WTF/, /Public Domain/
+];
+
+//------------------------------------------------------------------------------
+// Data
+//------------------------------------------------------------------------------
+
+var NODE_MODULES = "./node_modules/",
+    TEMP_DIR = "./tmp/",
+    BUILD_DIR = "./build/",
+
+    // Utilities - intentional extra space at the end of each string
+    MOCHA = NODE_MODULES + "mocha/bin/_mocha ",
+
+    // Files
+    MAKEFILE = "./Makefile.js",
+    /*eslint-disable no-use-before-define */
+    JS_FILES = find("lib/").filter(fileType("js")).join(" ") + " espree.js",
+    TEST_FILES = find("tests/lib/").filter(fileType("js")).join(" ");
+    /*eslint-enable no-use-before-define */
+
+//------------------------------------------------------------------------------
+// Helpers
+//------------------------------------------------------------------------------
+
+/**
+ * Generates a function that matches files with a particular extension.
+ * @param {string} extension The file extension (i.e. "js")
+ * @returns {Function} The function to pass into a filter method.
+ * @private
+ */
+function fileType(extension) {
+    return function(filename) {
+        return filename.substring(filename.lastIndexOf(".") + 1) === extension;
+    };
+}
+
+/**
+ * Creates a release version tag and pushes to origin.
+ * @param {string} type The type of release to do (patch, minor, major)
+ * @returns {void}
+ */
+function release(type) {
+    target.test();
+    exec("npm version " + type);
+    target.changelog();
+    exec("git push origin master --tags");
+    exec("npm publish");
+    target.gensite();
+    target.publishsite();
+}
+
+//------------------------------------------------------------------------------
+// Tasks
+//------------------------------------------------------------------------------
+
+target.all = function() {
+    target.test();
+};
+
+target.lint = function() {
+    var errors = 0,
+        lastReturn;
+
+    echo("Validating Makefile.js");
+    lastReturn = nodeCLI.exec("eslint", MAKEFILE);
+    if (lastReturn.code !== 0) {
+        errors++;
+    }
+
+    echo("Validating JavaScript files");
+    lastReturn = nodeCLI.exec("eslint", JS_FILES);
+    if (lastReturn.code !== 0) {
+        errors++;
+    }
+
+    // echo("Validating JavaScript test files");
+    // lastReturn = nodeCLI.exec("eslint", TEST_FILES);
+    // if (lastReturn.code !== 0) {
+    //     errors++;
+    // }
+
+    if (errors) {
+        exit(1);
+    }
+};
+
+target.test = function() {
+    target.lint();
+
+    var errors = 0,
+        lastReturn;
+
+    // exec(ISTANBUL + " cover " + MOCHA + "-- -c " + TEST_FILES);
+    lastReturn = nodeCLI.exec("istanbul", "cover", MOCHA, "-- -c", TEST_FILES);
+    if (lastReturn.code !== 0) {
+      errors++;
+    }
+
+    // exec(ISTANBUL + "check-coverage --statement 99 --branch 98 --function 99 --lines 99");
+    lastReturn = nodeCLI.exec("istanbul", "check-coverage", "--statement 99 --branch 98 --function 99 --lines 99");
+    if (lastReturn.code !== 0) {
+      errors++;
+    }
+
+    target.browserify();
+
+    lastReturn = nodeCLI.exec("mocha-phantomjs", "-R dot", "tests/tests.htm");
+    if (lastReturn.code !== 0) {
+      errors++;
+    }
+
+    if (errors) {
+        exit(1);
+    }
+
+    target.checkLicenses();
+};
+
+target.docs = function() {
+    echo("Generating documentation");
+    nodeCLI.exec("jsdoc", "-d jsdoc lib");
+    echo("Documentation has been output to /jsdoc");
+};
+
+target.browserify = function() {
+
+    // 1. create temp and build directory
+    if (!test("-d", TEMP_DIR)) {
+        mkdir(TEMP_DIR);
+        mkdir(TEMP_DIR + "/lib");
+    }
+
+    if (!test("-d", BUILD_DIR)) {
+        mkdir(BUILD_DIR);
+    }
+
+    // 2. copy files into temp directory
+    cp("-r", "lib/*", TEMP_DIR + "/lib");
+    cp("espree.js", TEMP_DIR);
+
+    // 3. browserify the temp directory
+    nodeCLI.exec("browserify", TEMP_DIR + "espree.js", "-o", BUILD_DIR + "espree.js", "-s espree");
+
+    // 4. remove temp directory
+    rm("-r", TEMP_DIR);
+};
+
+target.changelog = function() {
+
+    // get most recent two tags
+    var tags = exec("git tag", { silent: true }).output.trim().split(/\s/g),
+        rangeTags = tags.slice(tags.length - 2),
+        now = new Date(),
+        timestamp = dateformat(now, "mmmm d, yyyy");
+
+    // output header
+    (rangeTags[1] + " - " + timestamp + "\n").to("CHANGELOG.tmp");
+
+    // get log statements
+    var logs = exec("git log --pretty=format:\"* %s (%an)\" " + rangeTags.join(".."), {silent: true}).output.split(/\n/g);
+    logs = logs.filter(function(line) {
+        return line.indexOf("Merge pull request") === -1 && line.indexOf("Merge branch") === -1;
+    });
+    logs.push(""); // to create empty lines
+    logs.unshift("");
+
+    // output log statements
+    logs.join("\n").toEnd("CHANGELOG.tmp");
+
+    // switch-o change-o
+    cat("CHANGELOG.tmp", "CHANGELOG.md").to("CHANGELOG.md.tmp");
+    rm("CHANGELOG.tmp");
+    rm("CHANGELOG.md");
+    mv("CHANGELOG.md.tmp", "CHANGELOG.md");
+
+    // add into commit
+    exec("git add CHANGELOG.md");
+    exec("git commit --amend --no-edit");
+
+};
+
+
+target.checkLicenses = function() {
+
+    function isPermissible(dependency) {
+        var licenses = dependency.licenses;
+
+        if (Array.isArray(licenses)) {
+            return licenses.some(function(license) {
+                return isPermissible({
+                    name: dependency.name,
+                    licenses: license
+                });
+            });
+        }
+
+        return OPEN_SOURCE_LICENSES.some(function(license) {
+            return license.test(licenses);
+        });
+    }
+
+    echo("Validating licenses");
+
+    checker.init({
+        start: __dirname
+    }, function(deps) {
+        var impermissible = Object.keys(deps).map(function(dependency) {
+            return {
+                name: dependency,
+                licenses: deps[dependency].licenses
+            };
+        }).filter(function(dependency) {
+            return !isPermissible(dependency);
+        });
+
+        if (impermissible.length) {
+            impermissible.forEach(function (dependency) {
+                console.error("%s license for %s is impermissible.",
+                    dependency.licenses,
+                    dependency.name
+                );
+            });
+            exit(1);
+        }
+    });
+};
+
+target.patch = function() {
+    release("patch");
+};
+
+target.minor = function() {
+    release("minor");
+};
+
+target.major = function() {
+    release("major");
+};
