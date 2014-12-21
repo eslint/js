f67d8860a2ac0807f6a2042b1266d82a6465a959
Nicholas C. Zakas <nicholas@nczconsulting.com>
2014-12-21 09:57:22 -0800

    Update: Move Messages to separate file

--- espree.js
+++ espree.js
@@ -38,13 +38,13 @@ var syntax = require("./lib/syntax"),
     tokenInfo = require("./lib/token-info"),
     astNodeTypes = require("./lib/ast-node-types"),
     SourceLocation = require("./lib/locations").SourceLocation,
-    defaultFeatures = require("./lib/features");
+    defaultFeatures = require("./lib/features"),
+    Messages = require("./lib/messages");
 
 var Token = tokenInfo.Token,
     TokenName = tokenInfo.TokenName,
     FnExprTokens = tokenInfo.FnExprTokens,
     PropertyKind,
-    Messages,
     SyntaxTreeDelegate,
     source,
     strict,
@@ -63,43 +63,6 @@ PropertyKind = {
     Set: 4
 };
 
-// Error messages should be identical to V8.
-Messages = {
-    UnexpectedToken: "Unexpected token %0",
-    UnexpectedNumber: "Unexpected number",
-    UnexpectedString: "Unexpected string",
-    UnexpectedIdentifier: "Unexpected identifier",
-    UnexpectedReserved: "Unexpected reserved word",
-    UnexpectedEOS: "Unexpected end of input",
-    NewlineAfterThrow: "Illegal newline after throw",
-    InvalidRegExp: "Invalid regular expression",
-    InvalidRegExpFlag: "Invalid regular expression flag",
-    UnterminatedRegExp: "Invalid regular expression: missing /",
-    InvalidLHSInAssignment: "Invalid left-hand side in assignment",
-    InvalidLHSInForIn: "Invalid left-hand side in for-in",
-    MultipleDefaultsInSwitch: "More than one default clause in switch statement",
-    NoCatchOrFinally: "Missing catch or finally after try",
-    UnknownLabel: "Undefined label '%0'",
-    Redeclaration: "%0 '%1' has already been declared",
-    IllegalContinue: "Illegal continue statement",
-    IllegalBreak: "Illegal break statement",
-    IllegalReturn: "Illegal return statement",
-    StrictModeWith: "Strict mode code may not include a with statement",
-    StrictCatchVariable: "Catch variable may not be eval or arguments in strict mode",
-    StrictVarName: "Variable name may not be eval or arguments in strict mode",
-    StrictParamName: "Parameter name eval or arguments is not allowed in strict mode",
-    StrictParamDupe: "Strict mode function may not have duplicate parameter names",
-    StrictFunctionName: "Function name may not be eval or arguments in strict mode",
-    StrictOctalLiteral: "Octal literals are not allowed in strict mode.",
-    StrictDelete: "Delete of an unqualified identifier in strict mode.",
-    StrictDuplicateProperty: "Duplicate data property in object literal not allowed in strict mode",
-    AccessorDataProperty: "Object literal may not have data and accessor property with the same name",
-    AccessorGetSet: "Object literal may not have multiple get/set accessors with the same name",
-    StrictLHSAssignment: "Assignment to eval or arguments is not allowed in strict mode",
-    StrictLHSPostfix: "Postfix increment/decrement may not have eval or arguments operand in strict mode",
-    StrictLHSPrefix: "Prefix increment/decrement may not have eval or arguments operand in strict mode",
-    StrictReservedWord: "Use of future reserved word in strict mode"
-};
 
 // Ensure the condition is true, otherwise throw an error.
 // This is only to have a better contract semantic, i.e. another safety net
