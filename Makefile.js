7d03d46b675101fea9f051baed0c16b9174ff6e9
Nicholas C. Zakas <nicholas@nczconsulting.com>
2014-12-29 11:36:47 -0800

    Build: Update release script

--- Makefile.js
+++ Makefile.js
@@ -60,23 +60,38 @@ function fileType(extension) {
 }
 
 /**
+ * Executes a command and returns the output instead of printing it to stdout.
+ * @param {string} cmd The command string to execute.
+ * @returns {string} The result of the executed command.
+ */
+function execSilent(cmd) {
+    return exec(cmd, { silent: true }).output;
+}
+
+/**
  * Creates a release version tag and pushes to origin.
  * @param {string} type The type of release to do (patch, minor, major)
  * @returns {void}
  */
 function release(type) {
+    var newVersion;
+
     target.test();
-    exec("npm version " + type);
+    newVersion = execSilent("npm version " + type).trim();
     target.changelog();
 
     // add changelog to commit
     exec("git add CHANGELOG.md");
     exec("git commit --amend --no-edit");
 
-    // publish all the things
+    // replace existing tag
+    exec("git tag -f " + newVersion);
+
+    // push all the things
     exec("git push origin master --tags");
     exec("npm publish");
 }
+
 
 /**
  * Splits a command result to separate lines.
