491f879aa2b0a71cd99a48a185698c894e8e637a
Nicholas C. Zakas <nicholas@nczconsulting.com>
2015-04-28 17:33:42 -0700

    Fix: Don't allow arrow function rest param (fixes #130)

---
+++ tests/fixtures/ecma-features-mix/spread-and-arrowFunctions/invalid-arrow-rest-params.result.js
@@ -0,0 +1,8 @@
+// line/column is inaccurate due to parser logic, but it's close enough
+module.exports = {
+    "index": 17,
+    "lineNumber": 1,
+    "column": 18,
+    "description": "Unexpected token .",
+    "message": "Error: Line 1: Unexpected token ."
+};
