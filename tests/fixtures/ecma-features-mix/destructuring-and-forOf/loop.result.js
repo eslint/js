98e5fd94958d26890fc18b39179d6a408f038f5e
Nicholas C. Zakas <nicholas@nczconsulting.com>
2015-09-15 13:47:17 -0700

    Fix: Ensure node type is correct for destructured (fixes #195)

---
+++ tests/fixtures/ecma-features-mix/destructuring-and-forOf/loop.result.js
@@ -0,0 +1,109 @@
+module.exports = {
+  "type": "Program",
+  "body": [
+    {
+      "type": "ForOfStatement",
+      "left": {
+        "type": "ArrayPattern",
+        "elements": [
+          {
+            "type": "Identifier",
+            "name": "a",
+            "range": [
+              6,
+              7
+            ],
+            "loc": {
+              "start": {
+                "line": 1,
+                "column": 6
+              },
+              "end": {
+                "line": 1,
+                "column": 7
+              }
+            }
+          }
+        ],
+        "range": [
+          5,
+          8
+        ],
+        "loc": {
+          "start": {
+            "line": 1,
+            "column": 5
+          },
+          "end": {
+            "line": 1,
+            "column": 8
+          }
+        }
+      },
+      "right": {
+        "type": "Identifier",
+        "name": "foo",
+        "range": [
+          12,
+          15
+        ],
+        "loc": {
+          "start": {
+            "line": 1,
+            "column": 12
+          },
+          "end": {
+            "line": 1,
+            "column": 15
+          }
+        }
+      },
+      "body": {
+        "type": "EmptyStatement",
+        "range": [
+          16,
+          17
+        ],
+        "loc": {
+          "start": {
+            "line": 1,
+            "column": 16
+          },
+          "end": {
+            "line": 1,
+            "column": 17
+          }
+        }
+      },
+      "range": [
+        0,
+        17
+      ],
+      "loc": {
+        "start": {
+          "line": 1,
+          "column": 0
+        },
+        "end": {
+          "line": 1,
+          "column": 17
+        }
+      }
+    }
+  ],
+  "sourceType": "script",
+  "range": [
+    0,
+    17
+  ],
+  "loc": {
+    "start": {
+      "line": 1,
+      "column": 0
+    },
+    "end": {
+      "line": 1,
+      "column": 17
+    }
+  }
+};
