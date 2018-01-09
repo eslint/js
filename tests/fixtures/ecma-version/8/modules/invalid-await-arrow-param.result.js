4160aeef7c46cd56e0bbd3f5b3847e5f8266c9fb
Toru Nagashima <star.ctor@gmail.com>
2018-01-09 13:26:34 +0900

    Upgrade: acorn v5.3.0 (#365)

--- tests/fixtures/ecma-version/8/modules/invalid-await-arrow-param.result.js
+++ tests/fixtures/ecma-version/8/modules/invalid-await-arrow-param.result.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "message": "The keyword 'await' is reserved",
+    "message": "Can not use keyword 'await' outside an async function",
     "column": 7,
     "index": 6,
     "lineNumber": 1
-}
+};
--- tests/fixtures/ecma-version/8/modules/invalid-await-named-destructured-array-param.result.js
+++ tests/fixtures/ecma-version/8/modules/invalid-await-named-destructured-array-param.result.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "message": "The keyword 'await' is reserved",
+    "message": "Can not use keyword 'await' outside an async function",
     "column": 9,
     "index": 8,
     "lineNumber": 1
-}
+};
