acb8776d369abf9e02f79142879e9b1a4774f938
Kai Cataldo <kai@kaicataldo.com>
2020-02-19 14:39:34 -0500

    Update: add latestEcmaVersion & supportedEcmaVersions (#430)
    * Update: add latestEcmaVersion() & supportedEcmaVersions()
    
    * Address feedback
    
    * Change public API from getter methods to static property

---
+++ tests/lib/supported-ecmaversions.js
@@ -0,0 +1,32 @@
+/**
+ * @fileoverview Tests for latestEcmaVersion & supportedEcmaVersions.
+ * @author Kai Cataldo
+ */
+
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const assert = require("assert"),
+    espree = require("../../espree");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+describe("latestEcmaVersion", () => {
+    it("should return the latest supported ecmaVersion", () => {
+        assert.strictEqual(espree.latestEcmaVersion, 11);
+    });
+});
+
+describe("supportedEcmaVersions", () => {
+    it("should return an array of all supported versions", () => {
+        assert.deepStrictEqual(
+            espree.supportedEcmaVersions,
+            [3, 5, 6, 7, 8, 9, 10, 11]
+        );
+    });
+});
