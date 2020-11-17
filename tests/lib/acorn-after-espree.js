3744059b17dbac9dc4c804049f2f12b286f09817
Mike Reinstein <mreinstein@sellpoints.com>
2020-11-17 08:59:59 -0800

    Upgrade: acorn 8.0.4

--- tests/lib/acorn-after-espree.js
+++ tests/lib/acorn-after-espree.js
@@ -19,10 +19,10 @@ const acorn = require("acorn"),
 
 describe("acorn", () => {
     it("acorn.parse() should work after espree was loaded.", () => {
-        const before = acorn.parse("var foo = bar /*world*/;");
+        const before = acorn.parse("var foo = bar /*world*/;", {ecmaVersion: 5});
 
         require("../../espree");
-        const after = acorn.parse("var foo = bar /*world*/;");
+        const after = acorn.parse("var foo = bar /*world*/;", {ecmaVersion: 5});
 
         assert.deepStrictEqual(after, before);
     });
