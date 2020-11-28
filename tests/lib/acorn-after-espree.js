d5ec4fc86b5d6bb05d1020202a2b9f1125d0199d
Mike Reinstein <mreinstein@sellpoints.com>
2020-11-27 16:33:40 -0800

    Fix: test for breaking behavior in acorn@8 https://github.com/acornjs/acorn/commit/85f41501c536d9141e50f616f5db6b37e5f0b31b

--- tests/lib/acorn-after-espree.js
+++ tests/lib/acorn-after-espree.js
@@ -26,4 +26,23 @@ describe("acorn", () => {
 
         assert.deepStrictEqual(after, before);
     });
+
+    it("acorn.parse() should not re-use the same identifier node in object patterns", () => {
+        const code = "({x, y = 1} = {z})";
+
+        const ast = acorn.parse(code, { ecmaVersion: 2015 });
+
+        const assignment = ast.body[0].expression;
+        const objectPattern = assignment.left;
+        const objectExpression = assignment.right;
+
+        const propertyX = objectPattern.properties[0];
+        const propertyY = objectPattern.properties[1];
+        const propertyZ = objectExpression.properties[0];
+
+        assert.notStrictEqual(propertyX.key, propertyX.value); // true in v7, false in v8
+        assert.notStrictEqual(propertyY.key, propertyY.value.left); // true in v7, false in v8
+        assert.notStrictEqual(propertyZ.key, propertyZ.value); // true in v7, false in v8
+
+    });
 });
