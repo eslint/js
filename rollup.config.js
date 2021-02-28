0cb01c0ad55400a46414ac13fd0d304a668acb50
Mike <reinstein.mike@gmail.com>
2021-02-27 16:37:05 -0800

    Build: add note explaining lib/version.js and lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,14 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve  from '@rollup/plugin-node-resolve';
-import json     from '@rollup/plugin-json';
-import fs       from 'fs';
+import commonjs from "@rollup/plugin-commonjs";
+import resolve  from "@rollup/plugin-node-resolve";
+import json     from "@rollup/plugin-json";
+import fs       from "fs";
 
 
-const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-fs.writeFileSync('lib/version.js', `const version = "${pkg.version}";\n\nexport default version;\n`);
+// the espree package version is exported from the module (i.e., import { version } from "espree")
+// read version from the package.json metadata and write it into lib/version.js at build time,
+// since esm cannot import json by default
+const pkg = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+fs.writeFileSync("lib/version.js", `const version = "${pkg.version}";\n\nexport default version;\n`);
 
 export default {
   input: "espree.js",
