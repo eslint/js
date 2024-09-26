1da6f0c7b9d13dd80e2327e2c4bd25b8281076b5
amaresh.sm <amareshsm13@gmail.com>
2024-09-27 00:26:16 +0530

    lint fix

--- packages/eslint-scope/lib/util/assert.js
+++ packages/eslint-scope/lib/util/assert.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview - This file provides a utility function `assert` that checks whether a given condition is true
+ * @fileoverview This file provides a utility function `assert` that checks whether a given condition is true
  * and throws an error with a specified message if the condition is false.
  * @author Amaresh S M
  */
@@ -10,11 +10,9 @@
  * @param {boolean} condition - The condition that is being asserted. If `false`, an error will be thrown.
  * @param {string} [message="Assertion failed."] - The error message that will be thrown if the condition is false.
  * @returns {void}
- *
  * @throws {Error} Throws an error if the condition is not met.
  */
-
-export function assert(condition, message = "Assertion failed.") {
+ export function assert(condition, message = "Assertion failed.") {
     if (!condition) {
         throw new Error(message);
     }
