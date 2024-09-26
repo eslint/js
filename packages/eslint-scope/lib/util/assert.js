a4d6d889ec2d051b520d48be5bf6b584530b8a9c
amaresh.sm <amareshsm13@gmail.com>
2024-09-27 00:32:36 +0530

    lint fix

--- packages/eslint-scope/lib/util/assert.js
+++ packages/eslint-scope/lib/util/assert.js
@@ -6,13 +6,12 @@
 
 /**
  * Asserts that a condition is true. If the condition is false, an error is thrown with the provided message.
- *
- * @param {boolean} condition - The condition that is being asserted. If `false`, an error will be thrown.
- * @param {string} [message="Assertion failed."] - The error message that will be thrown if the condition is false.
+ * @param {boolean} condition The condition that is being asserted. If `false`, an error will be thrown.
+ * @param {string} [message="Assertion failed."] The error message that will be thrown if the condition is false.
  * @returns {void}
  * @throws {Error} Throws an error if the condition is not met.
  */
- export function assert(condition, message = "Assertion failed.") {
+ export function assert(condition, message ="Assertion failed.") {
     if (!condition) {
         throw new Error(message);
     }
