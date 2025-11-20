9acbf0ddf5836cff71e7c5c14e2b72c1a9f81a9d
Francesco Trotta <github@fasttime.org>
2025-11-20 16:39:54 +0100

    feat: add types to ESLint Scope

---
+++ packages/eslint-scope/tests/types/types.test.ts
@@ -0,0 +1,410 @@
+/**
+ * @fileoverview This file contains code to test the ESLint Scope types.
+ * It was initially extracted from the DefinitelyTyped repository.
+ */
+
+/*
+ * MIT License
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE
+ */
+
+import * as eslintScope from "eslint-scope";
+import type { AnalyzeOptions } from "eslint-scope";
+import * as espree from "espree";
+import * as estree from "estree";
+
+const code = `
+function example() {
+  let x = 1;
+  console.log(x);
+}
+`;
+
+const ast = espree.parse(code, { ecmaVersion: 2022, sourceType: "module" }) as estree.Program;
+
+// $ExpectType ScopeManager
+const scopeManager = eslintScope.analyze(
+    ast,
+    {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        ignoreEval: true,
+        nodejsScope: false,
+        impliedStrict: false,
+        childVisitorKeys: null,
+        fallback: "iteration",
+    } satisfies AnalyzeOptions,
+);
+
+// $ExpectType GlobalScope
+scopeManager.globalScope;
+// $ExpectType  Scope<Variable<Reference>, Reference>[]
+scopeManager.scopes;
+
+// $ExpectType Scope<Variable<Reference>, Reference> | null
+const scope = scopeManager.acquire(ast);
+
+// $ExpectType Scope<Variable<Reference>, Reference> | null
+scopeManager.release(ast);
+
+if (scope) {
+    ((
+        type:
+            | "function"
+            | "module"
+            | "block"
+            | "catch"
+            | "class"
+            | "class-field-initializer"
+            | "class-static-block"
+            | "for"
+            | "function-expression-name"
+            | "global"
+            | "switch"
+            | "with",
+    ) => type satisfies typeof scope.type);
+    // $ExpectType boolean
+    scope.isStrict;
+    // $ExpectType Scope<Variable<Reference>, Reference> | null
+    scope.upper;
+    // $ExpectType Scope<Variable<Reference>, Reference>
+    scope.variableScope;
+    // $ExpectType Variable<Reference>[]
+    scope.variables;
+    // $ExpectType Reference[]
+    scope.references;
+    // $ExpectType  Scope<Variable<Reference>, Reference>[]
+    scope.childScopes;
+    // $ExpectType Node
+    scope.block;
+    // $ExpectType boolean
+    scope.functionExpressionScope;
+    // $ExpectType Reference[]
+    scope.implicit.left;
+    // $ExpectType Map<string, Variable<Reference>>
+    scope.implicit.set;
+    // $ExpectType Variable<Reference>[]
+    scope.implicit.variables;
+    // $ExpectType  Map<string, Variable>
+    scope.set;
+    // $ExpectType Reference[]
+    scope.through;
+}
+
+const variable = scope?.variables[0];
+if (variable) {
+    // $ExpectType string
+    variable.name;
+    // $ExpectType Scope<Variable<Reference>, Reference>
+    variable.scope;
+    // $ExpectType Identifier[]
+    variable.identifiers;
+    // $ExpectType Reference[]
+    variable.references;
+    // $ExpectType Definition[]
+    variable.defs;
+}
+
+const reference = scope?.references[0];
+if (reference) {
+    // $ExpectType Identifier
+    reference.identifier;
+    // $ExpectType Variable<Reference> | null
+    reference.resolved;
+    // $ExpectType () => boolean
+    reference.isWrite;
+    // $ExpectType () => boolean
+    reference.isRead;
+    // $ExpectType Scope<Variable<Reference>, Reference>
+    reference.from;
+}
+
+const definition = variable?.defs[0];
+if (definition) {
+    ((
+        type:
+            | "CatchClause"
+            | "ClassName"
+            | "FunctionName"
+            | "ImplicitGlobalVariable"
+            | "ImportBinding"
+            | "Parameter"
+            | "Variable",
+    ) => type satisfies typeof definition.type);
+    // $ExpectType Identifier
+    definition.name;
+    // $ExpectType ImportDeclaration | VariableDeclaration | null
+    definition.parent;
+}
+
+// $ExpectType GlobalScope
+const globalScope = scopeManager.globalScope;
+// $ExpectType 'global'
+globalScope.type;
+
+// $ExpectType ScopeManager
+eslintScope.analyze(ast);
+
+const blockScope = new eslintScope.BlockScope(scopeManager, scopeManager.globalScope, ast);
+// $ExpectType "block"
+blockScope.type;
+
+const identifier: estree.Identifier = {
+    type: "Identifier",
+    name: "foo",
+};
+const definition2 = new eslintScope.Definition(
+    "Variable",
+    identifier,
+    ast,
+    null,
+    null,
+    "let",
+);
+((
+    type:
+        | "CatchClause"
+        | "ClassName"
+        | "FunctionName"
+        | "ImplicitGlobalVariable"
+        | "ImportBinding"
+        | "Parameter"
+        | "Variable",
+) => type satisfies typeof definition2.type);
+// $ExpectType Identifier
+definition2.name;
+
+const functionScope = new eslintScope.FunctionScope(scopeManager, scopeManager.globalScope, ast, false);
+// $ExpectType "function"
+functionScope.type;
+// $ExpectType boolean
+functionScope.isArgumentsMaterialized();
+// $ExpectType boolean
+functionScope.isThisMaterialized();
+
+const globalScopeInstance = new eslintScope.GlobalScope(scopeManager, ast);
+// $ExpectType "global"
+globalScopeInstance.type;
+
+const moduleScope = new eslintScope.ModuleScope(scopeManager, scopeManager.globalScope, ast);
+// $ExpectType "module"
+moduleScope.type;
+
+const ref = new eslintScope.Reference(
+    identifier,
+    scopeManager.globalScope,
+    0,
+    null,
+    false,
+    false,
+    false,
+);
+// $ExpectType Identifier
+ref.identifier;
+// $ExpectType Scope<Variable<Reference>, Reference>
+ref.from;
+// $ExpectType boolean
+ref.isRead();
+// $ExpectType boolean
+ref.isWrite();
+// $ExpectType boolean
+ref.isReadOnly();
+// $ExpectType boolean
+ref.isWriteOnly();
+// $ExpectType boolean
+ref.isReadWrite();
+
+const scopeInstance = new eslintScope.Scope(
+    scopeManager,
+    "block",
+    null,
+    ast,
+    false,
+);
+((
+    type:
+        | "function"
+        | "module"
+        | "block"
+        | "catch"
+        | "class"
+        | "class-field-initializer"
+        | "class-static-block"
+        | "for"
+        | "function-expression-name"
+        | "global"
+        | "switch"
+        | "with",
+) => type satisfies typeof scopeInstance.type);
+// $ExpectType boolean
+scopeInstance.isStrict;
+// $ExpectType Scope<Variable<Reference>, Reference> | null
+scopeInstance.upper;
+// $ExpectType Scope<Variable<Reference>, Reference>
+scopeInstance.variableScope;
+// $ExpectType Variable<Reference>[]
+scopeInstance.variables;
+// $ExpectType Reference[]
+scopeInstance.references;
+// $ExpectType Scope<Variable<Reference>, Reference>[]
+scopeInstance.childScopes;
+// $ExpectType Node
+scopeInstance.block;
+// $ExpectType boolean
+scopeInstance.functionExpressionScope;
+// $ExpectType { left: Reference[]; set: Map<string, Variable<Reference>>; variables: Variable<Reference>[]; }
+scopeInstance.implicit;
+// $ExpectType Map<string, Variable>
+scopeInstance.set;
+// $ExpectType Map<string, Variable<Reference>>
+scopeInstance.taints;
+// $ExpectType Reference[]
+scopeInstance.through;
+// $ExpectType boolean
+scopeInstance.dynamic;
+// $ExpectType boolean
+scopeInstance.directCallToEvalScope;
+// $ExpectType boolean
+scopeInstance.thisFound;
+// $ExpectType void
+scopeInstance.resolve(ref);
+// $ExpectType boolean
+scopeInstance.isStatic();
+// $ExpectType boolean
+scopeInstance.isArgumentsMaterialized();
+// $ExpectType boolean
+scopeInstance.isThisMaterialized();
+// $ExpectType boolean
+scopeInstance.isUsedName("foo");
+
+const scopeManagerInstance = new eslintScope.ScopeManager({
+    ecmaVersion: 2022,
+    sourceType: "module",
+});
+// $ExpectType GlobalScope
+scopeManagerInstance.globalScope;
+// $ExpectType Scope<Variable<Reference>, Reference>[]
+scopeManagerInstance.scopes;
+// $ExpectType void
+scopeManagerInstance.addGlobals(["window", "self"]);
+// $ExpectType Scope<Variable<Reference>, Reference> | null
+scopeManagerInstance.acquire(ast);
+// $ExpectType Scope<Variable<Reference>, Reference>[] | null
+scopeManagerInstance.acquireAll(ast);
+// $ExpectType Scope<Variable<Reference>, Reference> | null
+scopeManagerInstance.release(ast);
+// $ExpectType Variable<Reference>[]
+scopeManagerInstance.getDeclaredVariables(ast);
+// $ExpectType boolean
+scopeManagerInstance.isGlobalReturn();
+// $ExpectType boolean
+scopeManagerInstance.isModule();
+// $ExpectType boolean
+scopeManagerInstance.isImpliedStrict();
+// $ExpectType boolean
+scopeManagerInstance.isStrictModeSupported();
+
+const variableInstance = new eslintScope.Variable("foo", scopeInstance);
+// $ExpectType string
+variableInstance.name;
+// $ExpectType Scope<Variable<Reference>, Reference>
+variableInstance.scope;
+// $ExpectType Identifier[]
+variableInstance.identifiers;
+// $ExpectType Reference[]
+variableInstance.references;
+// $ExpectType Definition[]
+variableInstance.defs;
+// $ExpectType boolean
+variableInstance.tainted;
+// $ExpectType boolean
+variableInstance.stack;
+
+let node: any;
+if (eslintScope.PatternVisitor.isPattern(node)) {
+    // $ExpectType Identifier | ObjectPattern | ArrayPattern | SpreadElement | RestElement | AssignmentPattern
+    node;
+}
+
+declare let rootPattern: estree.Pattern;
+
+// // $ExpectType PatternVisitor
+const patternVisitor = new eslintScope.PatternVisitor(
+    {
+        fallback: (node: any) => Object.keys(node).filter((key) => key !== "parent"),
+        childVisitorKeys: { TestExpression: ["argument"] },
+    },
+    rootPattern,
+    (pattern, misc) => {
+        // $ExpectType Identifier
+        pattern;
+        // $ExpectType AssignmentPattern[]
+        misc.assignments;
+        // $ExpectType boolean
+        misc.rest;
+        // $ExpectType boolean
+        misc.topLevel;
+    },
+);
+
+// $ExpectType Pattern
+patternVisitor.rootPattern;
+
+// $ExpectType PatternVisitorCallback
+patternVisitor.callback;
+
+// $ExpectType AssignmentPattern[]
+patternVisitor.assignments;
+
+// $ExpectType Expression[]
+patternVisitor.rightHandNodes;
+
+// $ExpectType RestElement[]
+patternVisitor.restElements;
+
+// $ExpectType (pattern: Identifier) => void
+patternVisitor.Identifier;
+
+// $ExpectType (pattern: Property) => void
+patternVisitor.Property;
+
+// $ExpectType (pattern: ArrayPattern) => void
+patternVisitor.ArrayPattern;
+
+// $ExpectType (pattern: AssignmentPattern) => void
+patternVisitor.AssignmentPattern;
+
+// $ExpectType (pattern: RestElement) => void
+patternVisitor.RestElement;
+
+// $ExpectType (pattern: MemberExpression) => void
+patternVisitor.MemberExpression;
+
+// $ExpectType (pattern: SpreadElement) => void
+patternVisitor.SpreadElement;
+
+// $ExpectType (pattern: SpreadElement) => void
+patternVisitor.ArrayExpression;
+
+// $ExpectType (pattern: AssignmentExpression) => void
+patternVisitor.AssignmentExpression;
+
+// $ExpectType (pattern: CallExpression) => void
+patternVisitor.CallExpression;
